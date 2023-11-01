52874c42eb9d218bc17721d4b09630a271c96921
s-ja <dkstmdwl0615@naver.com>
2023-11-01 13:02:48 +0900

    setting complete

--- workspace/ch01/ex01-13-02.ts
+++ workspace/ch01/ex01-13-02.ts
@@ -1,38 +1,37 @@
 // indexable 타입
-(function(){
+(function () {
   // TODO: User 인터페이스를 완성해서 실행하시오.
-  interface User{
-    name: string,
-    email: string
+  interface User {
+    name: string;
+    email: string;
   }
 
   var kim: User = {
-    name: '김철수',
-    email: 'kim@gmail.com',
+    name: "김철수",
+    email: "kim@gmail.com",
     phones: {
       home: {
-        num: '031...'
+        num: "031...",
       },
       mobile: {
-        num: '010...'
-      }
-    }
+        num: "010...",
+      },
+    },
   };
 
   var lee: User = {
-    name: '이영희',
-    email: 'lee@gmail.com',
+    name: "이영희",
+    email: "lee@gmail.com",
     phones: {
       office: {
-        num: '02...'
+        num: "02...",
       },
       mobile: {
         // number: '010...', // 이 주석을 제거하면 컴파일 에러가 발생해야 함
-        num: '010...'
-      }
-    }
+        num: "010...",
+      },
+    },
   };
 
   console.log(kim, lee);
 })();
-
